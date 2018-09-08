1c1cb02360c70a0830e3d7081909da3add344d45
faizalfernandi <42776197+faizalfernandi@users.noreply.github.com>
2018-09-08 14:17:33 +0700

    Add files via upload

--- tugas/Penjualan.java
+++ tugas/Penjualan.java
@@ -7,13 +7,13 @@ public class Penjualan {
 		short d = 10000;
 		short e = 5000;
 
-		int hasil = a+b+c+d+e;
+		double hasil = a+b+c+d+e;
 
 		System.out.println("Total Penjualan = " + hasil);
 		
 		int uang = 100000;
 
-		double kembalian = (double)uang - (double)hasil;
+		float kembalian = (float)uang - (float)hasil;
 
 		System.out.println("kembalian adalah = " + kembalian);
 
